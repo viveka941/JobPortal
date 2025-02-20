8ae095180c9d578a89864d2fe6bab53903608450
Vivek kumar <viveka9415@gmail.com>
2025-02-20 09:23:17 +0530

    feat: add application, company, and job models with Mongoose schemas

---
+++ Backend/models/application.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const applicationSchema = new mongoose.Schema({
+  job: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job",
+    required: true,
+  },
+  applicant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  status: {
+    type: mongoose.Schema.Types.ObjectId,
+    enum:['pending','accepted','jejected'],
+    default: 'pending'
+  },
+},{timestamps: true});
+export const Application = mongoose.model("Application",applicationSchema)
