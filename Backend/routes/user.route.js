0fbe627ce34e96f71f637d88ce8023ff8d907295
Vivek kumar <viveka9415@gmail.com>
2025-02-20 12:40:12 +0530

    feat: add logout route to user authentication

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   login,
+  logout,
   register,
   updateProfile,
 } from "../controllers/user.controller.js";
@@ -9,6 +10,7 @@ const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/logout").post(logout);
 router.route("/profile/update").post(authenticationToken, updateProfile);
 
 export default router;
