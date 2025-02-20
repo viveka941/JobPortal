52b9085846f1daa18ea6f5c7f4f8038062bbb852
Vivek kumar <viveka9415@gmail.com>
2025-02-20 20:02:21 +0530

    fix: enhance getAllCompanies response structure and correct updateCompany method to use findByIdAndUpdate

--- Backend/controllers/company.controller.js
+++ Backend/controllers/company.controller.js
@@ -54,6 +54,10 @@ export const getAllCompanies = async (req, res) => {
     if (!companies) {
       return res.status(404).json({ message: "No companies found" });
     }
+    return res.status(200).json({ 
+      companies,
+      success: true
+    })
   } catch (error) {
     console.error(error);
   }
@@ -88,7 +92,7 @@ export const updateCompany = async(req,res)=>{
 
     const updateData = { name, description, website, location };
 
-    const company = await Company.findById(req.params.id,updateData,{new: true})
+    const company = await Company.findByIdAndUpdate(req.params.id,updateData,{new: true})
 
 if(!company){
   return res.status(404).json({message:"Company not found"})
