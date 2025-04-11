9ed37f31e9022bbf0c8d35c53f5763cc1193f9ab
Vivek kumar <viveka9415@gmail.com>
2025-04-11 09:47:11 +0530

    Update CORS configuration to allow requests from production URL

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://job-portal-green-mu.vercel.app/"],
   credentials: true,
 };
 app.use(cors(corsOptions));
