4acc38255b1532bff270713c82621fcc2642fe20
Vivek kumar <viveka9415@gmail.com>
2025-04-11 09:48:47 +0530

    remove

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://job-portal-green-mu.vercel.app/"],
+  origin: ["https://job-portal-green-mu.vercel.app"],
   credentials: true,
 };
 app.use(cors(corsOptions));
