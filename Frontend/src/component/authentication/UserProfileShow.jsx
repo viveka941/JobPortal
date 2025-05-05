e5bf8f21c7f840228c4d4eeeab0e3729ee0db5e9
Vivek kumar <viveka9415@gmail.com>
2025-05-05 19:56:19 +0530

    Refactor Edit Profile button and comment out ChatBot component in UserProfileShow

--- Frontend/src/component/authentication/UserProfileShow.jsx
+++ Frontend/src/component/authentication/UserProfileShow.jsx
@@ -48,8 +48,7 @@ export default function UserProfileShow() {
             onClick={() => setOpenEdit(true)}
             className="bg-blue-300 p-2 rounded-full hover:bg-gray-400 transition"
           >
-            ✒
-            <span className="text-black">Edit Profile</span>
+            ✒<span className="text-black">Edit Profile</span>
           </button>
         </div>
 
@@ -145,7 +144,7 @@ export default function UserProfileShow() {
 
       {/* Edit Profile Modal */}
       <EditProfileModel open={openEdit} setOpen={setOpenEdit} />
-      <div className="fixed top-20 right-4"><ChatBot /></div>
+      {/* <div className="fixed top-20 right-4"><ChatBot /></div> */}
     </div>
   );
 }
