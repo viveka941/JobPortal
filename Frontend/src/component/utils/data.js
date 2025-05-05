09a8f18b9262c08ae8aa1cd69d70ef2e1c4cc117
Vivek kumar <viveka9415@gmail.com>
2025-05-05 09:15:51 +0530

    Update API endpoints to use production URLs instead of localhost

--- Frontend/src/component/utils/data.js
+++ Frontend/src/component/utils/data.js
@@ -1,13 +1,13 @@
-export const USER_API_ENDPOINT = "http://localhost:8000/api/users";
+export const USER_API_ENDPOINT = "https://jobportal-8pxk.onrender.com/api/users";
 
 
-export const JOB_API_ENDPOINT = "http://localhost:8000/api/job";
+export const JOB_API_ENDPOINT = "https://jobportal-8pxk.onrender.com/api/job";
 
 
 export const APPLICATION_API_ENDPOINT =
-  "http://localhost:8000/api/application";
+  "https://jobportal-8pxk.onrender.com/api/application";
 
-  export const COMPANY_API_ENDPOINT = "http://localhost:8000/api/company";
+  export const COMPANY_API_ENDPOINT = "https://jobportal-8pxk.onrender.com/api/company";
 
 
 
